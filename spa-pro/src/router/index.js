3406d5374e839ae61d41eb86665442db78785009
mozg3000 <i.luka@outlook.com>
2019-12-24 11:48:51 +0300

    npm i jointjs

--- spa-pro/src/router/index.js
+++ spa-pro/src/router/index.js
@@ -3,25 +3,31 @@ import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 import Shop from '../components/Shop.vue'
 import ShopAdd from '../components/ShopAdd.vue'
+import Map from "../views/Map";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/shop/:id',
-    name:'shop',
-    component: Shop,
-    props: true
-  },
+    {
+        path: '/',
+        name: 'home',
+        component: Home
+    },
+    {
+        path: '/shop/:id',
+        name:'shop',
+        component: Shop,
+        props: true
+    },
     {
         path: '/shop-add',
         name:'shopAdd',
         component: ShopAdd
+    },
+    {
+        path: '/map',
+        name:'map',
+        component: Map
     }
 ]
 
---
+++ spa-pro/src/views/Map.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <SVGComponent></SVGComponent>
+    </div>
+</template>
+
+<script>
+    import SVGComponent from "../components/SVGComponent";
+    export default {
+        name: "Map",
+        components: {
+            SVGComponent
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
