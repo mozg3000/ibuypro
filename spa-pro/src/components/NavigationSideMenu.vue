7a3ef2239f783da69a17d9b3dbfe79e9e0b41d3e
mozg3000 <i.luka@outlook.com>
2019-12-27 14:00:24 +0300

    Неудачная попытка выделать компонент с боковым меню

---
+++ spa-pro/src/components/NavigationSideMenu.vue
@@ -0,0 +1,99 @@
+<template>
+    <div>
+        <v-navigation-drawer
+                v-model="drawer"
+                :clipped="$vuetify.breakpoint.lgAndUp"
+                app
+        >
+            <v-list dense>
+                <template v-for="item in items">
+                    <v-layout
+                            v-if="item.heading"
+                            :key="item.heading"
+                            row
+                            align-center
+                    >
+                        <v-flex xs6>
+                            <v-subheader v-if="item.heading">
+                                {{ item.heading }}
+                            </v-subheader>
+                        </v-flex>
+                        <v-flex
+                                xs6
+                                class="text-xs-center"
+                        >
+                            <a
+                                    href="#!"
+                                    class="body-2 black--text"
+                            >EDIT</a>
+                        </v-flex>
+                    </v-layout>
+                    <v-list-group
+                            v-else-if="item.children"
+                            :key="item.text"
+                            v-model="item.model"
+                            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                            append-icon=""
+                    >
+                        <template v-slot:activator>
+                            <v-list-item>
+                                <v-list-item-content>
+                                    <v-list-item-title>
+                                        {{ item.text }}
+                                    </v-list-item-title>
+                                </v-list-item-content>
+                            </v-list-item>
+                        </template>
+                        <v-list-item
+                                v-for="(child, i) in item.children"
+                                :key="i"
+                                @click=""
+                        >
+                            <v-list-item-action v-if="child.icon">
+                                <v-icon>{{ child.icon }}</v-icon>
+                            </v-list-item-action>
+                            <v-list-item-content>
+                                <v-list-item-title>
+                                    {{ child.text }}
+                                </v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list-group>
+                    <v-list-item
+                            v-else
+                            :key="item.text"
+                            @click=""
+                    >
+                        <v-list-item-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>
+                                {{ item.text }}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NavigationSideMenu",
+        props: {
+            items: {
+                required: true,
+                type: Array,
+            }
+        },
+        data: ()=>({
+            drawer: null
+        })
+    }
+</script>
+
+<style scoped>
+
+</style>
