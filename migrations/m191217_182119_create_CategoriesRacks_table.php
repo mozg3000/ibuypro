a78a753c8dc7349a3913a39da429316a8edb863a
mozg3000 <i.luka@outlook.com>
2020-01-16 16:45:58 +0300

    Исправил миграцию m191217_182119_create_CategoriesRacks_table

--- migrations/m191217_182119_create_CategoriesRacks_table.php
+++ migrations/m191217_182119_create_CategoriesRacks_table.php
@@ -14,9 +14,22 @@ class m191217_182119_create_CategoriesRacks_table extends Migration
     {
         $this->createTable('{{%CategoriesRacks}}', [
             'id' => $this->primaryKey(),
-            'id_rack' => $this->addForeignKey(m191215_124058_create_racks_table::id),
-            'id_categories' => $this->addForeignKey(m191213_182113_create_categories_table::id)
+            'id_rack' => $this->integer()->notNull(),
+            'id_categories' => $this->integer()->notNull()
         ]);
+        $this->addForeignKey(
+            'fk-CategoryRacks_d',
+            'CategoriesRacks',
+            'id_categories',
+            'categories',
+            'id');
+        $this->addForeignKey(
+            'fk-CategoryRacks_fd',
+            'CategoriesRacks',
+            'id_rack',
+            'racks',
+            'id'
+        );
     }
 
     /**
